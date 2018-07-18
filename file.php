ff8349d484e648f6897c77e3b521cb8f5a938cde
nathanryan01 <110385567@umail.ucc.ie>
2018-07-18 21:47:53 +0100

    Changes made for how to handle each type of xml file

--- file.php
+++ file.php
@@ -9,12 +9,30 @@ header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
 require_once ("config.php");
 
+$data = json_decode( file_get_contents("php://input"),true );
+
+$number =  $data['value'];
+
+$file = " ";
+
+if($number == "100"){
+    $file = "test100.xml";
+}
+elseif($number == "1000"){
+    $file = "test1000.xml";
+}
+elseif($number == "10000"){
+    $file = "test10000.xml";
+}
+else{
+    $file = "test100000.xml";
+}
+
 $wrestler = array();
 
-$xml = simplexml_load_file("test.xml") or die("Error: Object Creation failure");
+$xml = simplexml_load_file($file) or die("Error: Object Creation failure");
 
 $xml = json_encode($xml);
-
 
 echo $xml;
 
